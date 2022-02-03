d7445d9eaddb50e966901e9f2ae8a432666072f9
Alex Meza <alex@mezainnovations.com>
2022-02-03 16:52:10 -0600

    Clear out old code to start from scratch

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
-# Minitwit - a MERN implementation
-
-```
-npm init -y
-npm install express body-parser cors mongoose
-npm install -g nodemon
-* install mongodb, make sure it is running *
-
-npm i bcryptjs body-parser concurrently express is-empty jsonwebtoken mongoose passport passport-jwt validator
-```
-
-`nodemon server`
+# Mini Twit in MERN
+AKA mernitwit
--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,25 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
+import logo from './logo.svg';
+import './App.css';
 
-import { Provider } from "react-redux";
-import store from "./store";
-
-import PostTwit from "./components/PostTwit";
-import EditTwit from "./components/EditTwit";
-import TwitFeed from "./components/TwitFeed";
-
-import Navbar from "./components/Navbar";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import PrivateRoute from "./components/private-route/PrivateRoute";
-import Dashboard from "./components/Dashboard";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-
-if (localStorage.jwtToken) {
-  const token = localStorage.jwtToken;
-  setAuthToken(token);
-  const decoded = jwt_decode(token);
-  store.dispatch(setCurrentUser(decoded));
-
-  const currentTime = Date.now() / 1000; // for ms
-  if (decoded.exp < currentTime) {
-    store.dispatch(logoutUser());
-    window.location.href = "./login";
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-          <Navbar />
-          <br />
-          <br />
-          <div className="container">
-            <Route exact path="/" component={TwitFeed} />
-            <Route path="/edit/:id" component={EditTwit} />
-            <Route path="/post" component={PostTwit} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Switch>
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            </Switch>
-          </div>
-          <br />
-          <br />
-        </Router>
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
